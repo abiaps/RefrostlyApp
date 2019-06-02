d11d923099875565ff46121024aff4af01e9bfe8
Kavudhami <abiandabi92@gmail.com>
2019-06-02 12:50:31 -0500

    Refrostly- version1.0.2 - Cleaned test directory

--- Refrostly/test/main/RefrostlyJUnitTest.java
+++ Refrostly/test/main/RefrostlyJUnitTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.test;
+package main;
 
 import main.Refrostly;
 import model.Inventory;
